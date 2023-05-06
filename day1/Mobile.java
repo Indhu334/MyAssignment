c04b0954d28cd2aaf30512f115d412b6ee5a4609
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-05-06 18:41:16 +0530

    Add files via upload
    Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Mobile {
+	public void gameing(){
+		System.out.println("gameing");
+	}
+	public void calling() {
+		System.out.println("calling");
+		
+	}
+	public void takephoto() {
+		System.out.println("takephoto");
+	}
+      public static void main(String[]args){
+    	  Mobile phone=new Mobile();
+    	  phone.gameing();
+    	  phone.calling();
+    	  phone.takephoto();
+    			  
+    			  
+      }
+}
+   
---
+++ day1/car.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class car {
+	public void Applybreak(){
+		System.out.println("Applybreak");
+	}
+	public void Closedoor() {
+		System.out.println("closedoor");
+	}
+	public void Wearsheetbelt(){
+		System.out.println("Wearsheetbelt");
+	}
+	public static void main(String[]arg){
+		car car=new car();
+		car.Applybreak();
+		car.Closedoor();
+		car.Wearsheetbelt();
+		
+		
+		
+	}
+
+}
