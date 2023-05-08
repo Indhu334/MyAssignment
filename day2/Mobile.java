7fa285b84fe98dcf061b4f769733f82b32764bd6
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-05-08 20:53:08 +0530

    Add files via upload
    Week1,day 2

---
+++ day2/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Mobile {
+	public static void main(String[]args){
+		String mobileBrandName="Apple";
+		System.out.println(mobileBrandName);
+		char mobileLogo='A';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=3;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=13;
+		System.out.println(modelNumber);
+		long mobilelmeiNumber=1234567L;
+		System.out.println( mobilelmeiNumber);
+		float mobilePrice=48000;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+		
+		
+	}
+
+}
---
+++ day2/Twowheeler.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class Twowheeler {
+	public static void main(String[]args){
+	int noOfWheels=4;
+	System.out.println(noOfWheels);
+	short noOfMirrors=2;
+	System.out.println(noOfMirrors);
+	long chassIsNumber=12345678L;
+	System.out.println(chassIsNumber);
+	boolean isPuncturated=true;
+	System.out.println(isPuncturated);
+	String bikeName="duke";
+	System.out.println(bikeName);
+	double runningKm=11.34;
+	System.out.println(runningKm);
+	}
+	
+	
+
+}
