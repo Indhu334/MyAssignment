f488247696ded4e37dfd39f057ae4a0ed3fff8d5
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-05-30 21:21:07 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,23 @@
+package week4.day2;
+
+
+	public class AxisBank extends BankInfo {
+
+		public void fixedDeposit() {
+			System.out.println("120");
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			AxisBank info = new AxisBank();
+			info.fixedDeposit();
+		}
+
+	}
+
+	
+
+
+	
+	
