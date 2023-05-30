6598d5f49deb66a98a188afa3fe8ae43e62fc862
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-05-30 21:10:41 +0530

    Add files via upload

---
+++ Collage.java
@@ -0,0 +1,23 @@
+package Org.Collage;
+
+public class Collage {
+	public void Collagecode() {
+		System.out.println("123");
+	}
+	public void CollageName() {
+		System.out.println("BIT");
+	}
+	public void CollageRank() {
+		System.out.println("3");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collage details=new Collage();
+		details.Collagecode();
+		details.CollageRank();
+		details.CollageName();
+
+	}
+
+}
---
+++ desktop.java
@@ -0,0 +1,16 @@
+package week4.day2;
+
+public class desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("123");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop call=new Desktop();
+		call.desktopSize();
+		call.Computermodel();
+
+	}
+
+}
