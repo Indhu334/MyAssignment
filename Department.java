c12b247078a126bf2bbffbb892741c92200dd649
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-05-30 21:11:09 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,18 @@
+package Org.Department;
+
+import Org.Collage.Collage;
+
+public class Department extends Collage{
+	public void DepartmentName() {
+		System.out.println("EIE");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department details2=new Department();
+		details2.DepartmentName();
+		
+
+	}
+
+}
