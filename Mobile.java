db7ca4c8220eca959973be801b4f44f9489bdb19
Indhu334 <132759597+Indhu334@users.noreply.github.com>
2023-07-18 20:58:11 +0530

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -1,23 +1,23 @@
-package week1.day1;
+package week1.day2;
 
 public class Mobile {
-	public void shareDocument(){
-		System.out.println("shareDocument");
-	}
-	public void sendMessage() {
-		System.out.println("sendMessage");
+	public static void main(String[]args){
+		String mobileBrandName="Apple";
+		System.out.println(mobileBrandName);
+		char mobileLogo='A';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=3;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=13;
+		System.out.println(modelNumber);
+		long mobilelmeiNumber=1234567L;
+		System.out.println( mobilelmeiNumber);
+		float mobilePrice=48000f;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+		
 		
 	}
-	public void makeCall() {
-		System.out.println("makeCall");
-	}
-      public static void main(String[]args){
-    	  Mobile phone=new Mobile();
-    	  phone.makeCall();
-    	  phone.sendMessage();
-    	  phone.shareDocument();
-    			  
-    			  
-      }
+
 }
-   
